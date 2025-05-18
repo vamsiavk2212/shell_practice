832cac9d7ca0ecb12046773a0804e235ead18f2e
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-18 06:11:10 +0530

    conversation-replaced

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hey $Person2, how are you?"
+echo "$Person2: Hello $Person1, I am fine. How are you doing"
+echo "$Person1: I am fine too. What's up?"
+echo "$Person2: Nothing, just going to Mars now, will you come?"
+echo "$Person1: Sorry, you carry on ! I will come once you come back"
