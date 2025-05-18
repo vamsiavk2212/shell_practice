d33bf43e21c0f5ce056166cdcd83b6dc98cfb425
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-18 10:14:10 +0530

    special-vars

--- 08-specialvars.sh
+++ 08-specialvars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables : $#"
+echo "Script name : $0"
+echo "Current Direcctory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
