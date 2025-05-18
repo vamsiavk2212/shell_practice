b1b6d18838ef5b8433faf78cb32dc1e7e38c60e4
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-18 05:53:36 +0530

    conversation started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey musk, how are you?"
+echo "Musk: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk: Nothing, just going to Mars now, will you come?"
+echo "Trump: Sorry, you carry on ! I will come once you come back"
