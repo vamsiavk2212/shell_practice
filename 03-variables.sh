9efe68a712bcd5fb89b6d9ad00546b1c03df3b5c
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-18 06:02:16 +0530

    conversation-replaced

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Trump
+Person2=Musk
+
+echo "$Person1: Hey $Person2, how are you?"
+echo "$Person2: Hello $Person1, I am fine. How are you doing"
+echo "$Person1: I am fine too. What's up?"
+echo "$Person2: Nothing, just going to Mars now, will you come?"
+echo "$Person1: Sorry, you carry on ! I will come once you come back"
