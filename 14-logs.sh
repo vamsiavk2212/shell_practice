ad7c6ae7077e4b64f98e57555839c27a767a21fc
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-24 10:03:04 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" &>>$LOG_FILE
